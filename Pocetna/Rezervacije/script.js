c2e86d008ce39066cc6ea62c5adb1e121aec5554
Erika Kljunic <kljunicerika@gmail.com>
2025-08-08 15:46:51 +0200

    Rezervacije gumb

--- Pocetna/Rezervacije/script.js
+++ Pocetna/Rezervacije/script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const dateInput = document.getElementById("res-date");
   const formSection = document.getElementById("res-form");
 
-  // Inicijalno sakrij formu
   formSection.style.display = "none";
 
   flatpickr(dateInput, {
@@ -10,19 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
     dateFormat: "Y-m-d",
     disable: [
       function (date) {
-        // omogući samo petak (5) i subotu (6)
         return !(date.getDay() === 5 || date.getDay() === 6);
       },
     ],
     locale: {
       firstDayOfWeek: 1,
     },
-    onReady: function (selectedDates, dateStr, instance) {
-      // kad se kalendar prikazuje, flatpickr automatski označi today
-    },
+    onReady: function (selectedDates, dateStr, instance) {},
     onChange: function (selectedDates) {
       if (selectedDates.length) {
-        // prikaz obrasca
         formSection.style.display = "block";
       } else {
         formSection.style.display = "none";
@@ -30,7 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  // plus/minus gostiju
   document.getElementById("minus-guest").addEventListener("click", () => {
     const g = document.getElementById("res-guests");
     if (+g.value > 1) g.value = +g.value - 1;
